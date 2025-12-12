fa53e56d2bcd6a6540832d5e5cfb3a09667690e3
Tea and Python <pythonictea@gmail.com>
2025-12-12 14:40:34 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -15,4 +13,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  base: "/", 
 }));
