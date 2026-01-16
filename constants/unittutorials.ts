7fae15499c1d5052c572b7ba9e19f37bc61788a9
Tea and Python <pythonictea@gmail.com>
2026-01-16 11:34:32 +0000

    Delete constants directory

--- constants/unittutorials.ts
+++
@@ -1,9 +0,0 @@
-import tutorial1 from '@/assets/tutorials/tutorial1.png';
-
-export const unitTutorials = [
-    {
-        title: 'Unit Tutorials',
-        content: 'Placeholders only',
-        image: tutorial1,
-    },
-];
